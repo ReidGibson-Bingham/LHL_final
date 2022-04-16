2dec67eb8d991aa496a813776aa8e8c380d21917
P4ulH1ll <jsr3b0rn@gmail.com>
2022-04-16 00:08:00 +0000

    adding models, routes and seeds

---
+++ app/controllers/texts_controller.rb
@@ -0,0 +1,13 @@
+class TextsController < ApplicationController
+
+  def index
+    @texts = Text.all 
+    render json: @texts
+  end
+
+  def show 
+    @text = Text.find params[:id]
+    render json: @text
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api do # /api/data
+  # namespace :api do # /api/data
 
-    get '/data', to: 'tests#index'
+  #   get '/data', to: 'tests#index'
     
-    resources :dogs
+  #   resources :dogs
 
-  end
+  # end
+    
+  resources :texts, except: [:destroy]
 
   get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
