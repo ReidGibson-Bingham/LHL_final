c465ebe89a5aa1db98d7aa2c27cbffe6deecce46
Kent Gulka <kentgulka@modindsoft.com>
2022-04-15 18:10:07 -0600

    Feature User Model-Initial
    Added initial User Model, Seeds and related Controller to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users, except: [:destroy]
+
   namespace :api do # /api/data
 
     get '/data', to: 'tests#index'
-    
+
+      
     resources :dogs
 
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{name: 'Marvin the Martian', email: "marvinmartian@looneytunes.com", password: "password"}, {name: 'Porky Pig', email: "porkypig@looneytunes.com", password: "password"}, {name: 'Tweety Bird', email: "tweetybird@looneytunes.com", password: "password"}, {name: 'Yosemite Sam', email: "yosemitesam@looneytunes.com", password: "password"}])
