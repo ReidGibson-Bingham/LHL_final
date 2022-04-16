6e12ea886d35a6c85afb8ce61a3fa36697eb0dae
Kent Gulka <kentgulka@modindsoft.com>
2022-04-16 17:33:56 -0600

    Feature Games Controller-actions
    added index and show actions for the games controller.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,10 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+    render json: @games
+  end
+  def show
+    @games = Game.find params[:id]
+    render json: @games
+  end
 end
