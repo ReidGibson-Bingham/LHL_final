3bf4b95d946d64e630853799a09ba4ad299ef1d9
P4ulH1ll <jsr3b0rn@gmail.com>
2022-04-16 02:04:38 +0000

    cleanup from merge
    changed the schema, the readme, the name of the database and removed comments from the route file

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, except: [:destroy]
-
-  namespace :api do # /api/data
-
-    get '/data', to: 'tests#index'
-
-      
-    resources :dogs
-
-  # end
-    
+   
   resources :texts, except: [:destroy]
 
   get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-#ActiveRecord::Schema.define(version: 2022_04_15_220500) do
 ActiveRecord::Schema.define(version: 2022_04_15_223824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-  end
   create_table "texts", force: :cascade do |t|
     t.string "difficulty"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
   end
 
 end
