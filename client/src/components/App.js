87300211a86421ac0647e593a74013b39cc25aab
Kent Gulka <kentgulka@modindsoft.com>
2022-04-19 10:58:56 -0600

    Restructured Client Folders
    created component, component/test, navigation and styles folders for source files.  Created new TopNavbar js using bootstrap for main navigation in the app.

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import "../styles/App.scss";
+
+import TopNavbar from "../navigation/TopNavbar.js";
+
+//import Form from "./Form";
+import TextShow from "../TextShow";
+
+export default function App() {
+  return (
+    <main className="layout">
+      <TopNavbar />
+      <section className="typing-text">
+        <div class="row">
+          <div class="col-sm-8">Mode and Difficulty Selection Here</div>
+          <div class="col-sm-4">Time and Errors Here</div>
+        </div>
+        <div className="App">Typing Text Here</div>
+        <div className="App">Keyboard Here</div>
+      </section>
+    </main>
+  );
+}
