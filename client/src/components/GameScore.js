e4c05606ab5533e19ed382be5f24c863381496e7
Kent Gulka <kentgulka@modindsoft.com>
2022-04-20 13:20:30 -0600

    Feature typing evaluation
    added GameScore component with the error count and game timer.

---
+++ client/src/components/GameScore.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import classNames from "classnames";
+
+export default function GameScore(props) {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
+  //<span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+  return (
+    <div className="stopwatch">
+      <div className="numbers">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+      </div>
+      <div className="errorCountStat">
+        <span>Error Count: {props.errorCount}</span>
+      </div>
+
+      <div className="buttons">
+        <button onClick={() => setRunning(true)}>Start</button>
+        <button onClick={() => setRunning(false)}>Stop</button>
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
+    </div>
+  );
+}
