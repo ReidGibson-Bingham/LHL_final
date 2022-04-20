bd241999fe71cfa0543512b548c01c1902983d1a
Kent Gulka <kentgulka@modindsoft.com>
2022-04-20 15:56:51 -0600

    Refactoring App
    Refactoring components to keep state at App level. Renamed TextShow to TypingText and moved axios call to App.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,27 +5,65 @@ import "../styles/App.scss";
 import useAppData from "../hooks/useAppData";
 import TopNavbar from "../navigation/TopNavbar.js";
 import TextShow from "../TextShow";
+import TypingText from "./TypingText";
+import GameScore from "./GameScore";
+import GameMode from "./GameMode";
 
 //import Form from "./Form";
 //import TextShow from "../TextShow";
 
 export default function App() {
-  const { errorCount, textId, textDifficultyId, typingTime } = useAppData();
+  //const { errorCount, textId, textDifficultyId, typingTime } = useAppData();
+  const [gameStatus, setGameStatus] = useState("new"); // statuses => new, started, done
+  const [errorCount, setErrorCount] = useState(0);
+  const [textId, setTextId] = useState(0);
+  const [typingText, setTypingText] = useState("no text".split(""));
+  const [textDifficulty, setTextDifficulty] = useState(0);
+
+  const setErrCount = (errorCount) => setErrorCount(errorCount);
+  const fetchData = (textDifficulty) => {
+    //console.log("fetch data ??");
+    const min = 1;
+    const max = 4;
+    const randNum = Math.floor(Math.random() * (max - min));
+    axios
+      .get("http://localhost:3000/texts") // You can simply make your requests to "/api/whatever you want"
+      .then((response) => {
+        // handle success
+        console.log("response data is", response.data); // The entire response from the Rails API
+        //console.log("randomnumber", randNum);
+        // console.log("response.data.content", response.data[randNum].content); // Just the message
+        setTypingText(response.data[randNum].content.split(""));
+        //return response.data[randNum].content.split("");
+      });
+    //console.log("text is:", text);
+  };
+  useEffect(() => {
+    //setTypingText(fetchData(textDifficulty));
+    //need to call this on textDifficulty selection.
+    fetchData(textDifficulty);
+  }, []);
 
   return (
     <main className="layout">
       <TopNavbar />
       <section className="typing-text">
         <div className="row">
-          <div className="col-sm-8">Mode and Difficulty Selection Here</div>
-          <div className="col-sm-4">Time and Errors Here</div>
+          <div className="col-sm-8">
+            <GameMode />
+          </div>
+          <div className="col-sm-4">
+            <GameScore />
+          </div>
         </div>
         <div className="App">Typing Text Here</div>
         <div className="App">
-          <TextShow
+          <TypingText
             errorCount={errorCount}
             textId={textId}
-            textDifficultyId={textDifficultyId}
+            text={typingText}
+            gameStatus={gameStatus}
+            //onChange={onChangeInput}
           />
         </div>
       </section>
--- client/src/components/TypingText.js
+++ client/src/components/TypingText.js
@@ -5,10 +5,11 @@ import axios from "axios";
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 
-import "./styles.css";
+//import "./styles.css";
+import "../styles/TypingText.scss";
 
-export default function TextShow(props) {
-  const [text, setText] = useState("loading text".split(""));
+export default function TypingText(props) {
+  //const [text, setText] = useState("loading text".split(""));
   //const [typing, setTyping] = useState("");
   //------------------------------------
   const [input, setInput] = useState("");
@@ -54,24 +55,6 @@ export default function TextShow(props) {
   //   setTyping(e.target.value);
   // };
 
-  const fetchData = (e) => {
-    e.preventDefault();
-    //console.log("fetch data ??");
-    const min = 1;
-    const max = 4;
-    const randNum = Math.floor(Math.random() * (max - min));
-    axios
-      .get("http://localhost:3000/texts") // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        //console.log("response data is", response.data); // The entire response from the Rails API
-        //console.log("randomnumber", randNum);
-        // console.log("response.data.content", response.data[randNum].content); // Just the message
-        setText(response.data[randNum].content.split(""));
-      });
-    //console.log("text is:", text);
-  };
-
   function check(letter, index) {
     let textTyped = input[index];
     // console.log(
@@ -88,11 +71,14 @@ export default function TextShow(props) {
       return "has-background-wrong";
     }
   }
-
+  //   <button id="text-button" onClick={fetchData}>
+  //   Click for showing typing text
+  // </button>
+  console.log("props.text:", props.text);
   return (
     <div className="TextShow">
       <div id="text-showed">
-        {text.map((letter, i) => (
+        {props.text.map((letter, i) => (
           <span key={i} className={check(letter, i)}>
             {letter}
           </span>
@@ -100,14 +86,12 @@ export default function TextShow(props) {
       </div>
 
       {/* Labels and inputs for form data */}
-      <button id="text-button" onClick={fetchData}>
-        Click for showing typing text
-      </button>
+
       <label className="label"></label>
       <textarea
         id="keyboard-input"
         value={input}
-        placeholder={"Tap on the virtual keyboard to start"}
+        placeholder={"Start typing to begin game"}
         onChange={onChangeInput}
       />
 
