1cfd7ecdec1c76860b030dabc22af6f2e1de4f7c
P4ulH1ll <jsr3b0rn@gmail.com>
2022-04-21 18:25:45 +0000

    merge changes

--- client/src/providers/GameProvider.js
+++ client/src/providers/GameProvider.js
@@ -6,16 +6,9 @@ export const gameContext = createContext();
 
 // Create a Component wrapper from Context.Provider
 export default function GameProvider(props) {
-<<<<<<< HEAD
-
-  const [gameStatus, setGameStatus] = useState("new"); // cstatuses => new, started, done
-
-  const [errorCount, setErrorCount] = useState(0);
-=======
   const [gameStatus, setGameStatus] = useState("new"); // statuses => new, started, done
   const [errorCount, setErrorCount] = useState(0);
 
->>>>>>> 4cf28a449d4c2e3d512a4377e5d3de8d6d2e0a31
   const [textId, setTextId] = useState(0);
   const [typingText, setTypingText] = useState("no text".split(""));
   const [textDifficulty, setTextDifficulty] = useState(0);
@@ -30,14 +23,8 @@ export default function GameProvider(props) {
     axios
       .get("http://localhost:3000/texts") // You can simply make your requests to "/api/whatever you want"
       .then((response) => {
-<<<<<<< HEAD
-        console.log("response data is", response.data); // The 
-        setTypingText(response.data[textDifficulty].content.trim().split(""));
-        console.log("text difficulty:", textDifficulty);
-=======
         console.log("response data is", response.data); // The
         setTypingText(response.data[textDifficulty].content.trim().split(""));
->>>>>>> 4cf28a449d4c2e3d512a4377e5d3de8d6d2e0a31
         //return response.data[randNum].content.split("");
       });
     //console.log("text is:", text);
@@ -49,9 +36,6 @@ export default function GameProvider(props) {
   }, []);
 
   // This list can get long with a lot of functions.  Reducer may be a better choice
-<<<<<<< HEAD
-  const providerData = { errorCount, setErrorCount, gameStatus, setGameStatus, textId, setTextId, typingText, fetchData, textDifficulty, setTextDifficulty};
-=======
   const providerData = {
     errorCount,
     setErrorCount,
@@ -66,7 +50,6 @@ export default function GameProvider(props) {
     gameTotalTime,
     setGameTotalTime,
   };
->>>>>>> 4cf28a449d4c2e3d512a4377e5d3de8d6d2e0a31
 
   // We can now use this as a component to wrap anything
   // that needs our state
