4cff3c95cadfa07efa084326b3ab898e8b7bc93f
P4ulH1ll <jsr3b0rn@gmail.com>
2022-04-23 18:27:26 +0000

    adding a condition for game mode

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import "../styles/App.scss";
 
-import { useContext } from "react";
+import { useState, useContext } from "react";
 
 import axios from "axios";
 
@@ -18,36 +18,12 @@ import GameProvider from "../providers/GameProvider";
 import ChatRoom from "./ChatRoom";
 
 export default function App() {
-  //const { errorCount, textId, textDifficultyId, typingTime } = useAppData();
-  // const [gameStatus, setGameStatus] = useState("new"); // statuses => new, started, done
-  // const [errorCount, setErrorCount] = useState(0);
-  // const [textId, setTextId] = useState(0);
-  // const [typingText, setTypingText] = useState("no text".split(""));
-  // const [textDifficulty, setTextDifficulty] = useState(0);
 
-  // const setErrCount = (errorCount) => setErrorCount(errorCount);
-  // const fetchData = (textDifficulty) => {
-  //   //console.log("fetch data ??");
-  //   const min = 1;
-  //   const max = 4;
-  //   const randNum = Math.floor(Math.random() * (max - min));
-  //   axios
-  //     .get("http://localhost:3000/texts") // You can simply make your requests to "/api/whatever you want"
-  //     .then((response) => {
-  //       // handle success
-  //       console.log("response data is", response.data); // The entire response from the Rails API
-  //       //console.log("randomnumber", randNum);
-  //       // console.log("response.data.content", response.data[randNum].content); // Just the message
-  //       setTypingText(response.data[randNum].content.split(""));
-  //       //return response.data[randNum].content.split("");
-  //     });
-  //   //console.log("text is:", text);
-  // };
-  // useEffect(() => {
-  //   //setTypingText(fetchData(textDifficulty));
-  //   //need to call this on textDifficulty selection.
-  //   fetchData(textDifficulty);
-  // }, []);
+  const [comptetiveMode, setCompetetiveMode] = useState(false)
+
+  const compStatusButton = function () {
+    setCompetetiveMode(true)
+  }
 
   return (
     <main className="layout">
@@ -55,9 +31,11 @@ export default function App() {
         <TopNavbar />
         <section className="typing-text">
           <div className="row">
+            
             <div className="col-sm-8">
               <GameMode />
             </div>
+            
             <div className="col-sm-4">
               <GameScore />
             </div>
@@ -73,8 +51,12 @@ export default function App() {
 
         <div className="row">
 
+          <div className="mode-button">
+            <button onClick={compStatusButton}>Competitive Mode</button>
+          </div>
+
           <div className="col-sm-12">
-            <ChatRoom />
+            {comptetiveMode && <ChatRoom />}
           </div>
           
         </div>
