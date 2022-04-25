c3795f47f7b23cf7ace6b791937222bce24f6155
Reid Gibson-Bingham <jsr3b0rn@gmail.com>
2022-04-25 16:05:45 -0700

    git commit

--- client/src/components/GameStats.js
+++ client/src/components/GameStats.js
@@ -1,14 +1,17 @@
-import React, { Fragment, useState, useContext, useEffect } from "react";
+import React, {Fragment, useState, useContext, useEffect} from "react";
 import axios from "axios";
 import AutoSave from "./AutoSave";
 
 //import "./styles.css";
-//import "../styles/GameStats.scss";
+import "../styles/GameStats.scss";
+
 
 import { gameContext } from "../providers/GameProvider";
 
 export default function GameStats() {
+
   const {
+
     textDifficulty,
     getGamesData,
     gameStatus,
@@ -17,16 +20,16 @@ export default function GameStats() {
     setGameData,
     gamesData,
     setGamesData,
+    
   } = useContext(gameContext);
 
   console.log("00 games data:", gamesData[0]);
 
   // only display current users
-  const gameItems = gamesData.map((game) => {
+  const gameItems = gamesData.map( (game) => {
     game = Object.values(game);
-
+    
     return (
-<<<<<<< HEAD
       <Fragment>
         <br></br>
         <h4 key= {Math.random()}>Game #: {game.slice(0, 1)} </h4>
@@ -52,9 +55,9 @@ export default function GameStats() {
     <Fragment>
       
       {console.log("gameStatus: ",gameStatus)}
-      <div className="GameStatus">
+      {/* <div className="GameStatus">
         {gameStatus === "done" && <AutoSave/> }
-      </div>
+      </div> */}
 
       <button className='stats-button' onClick={getGamesData}> get stats </button>
       
@@ -64,37 +67,6 @@ export default function GameStats() {
         {gameItems[gameItems.length-1]}
       </ul>
     </Fragment>
-=======
-      <div key={game[0]}>
-        Error Count: {game.slice(2, 3)}
-        <br></br>
-        Time (seconds): {game.slice(3, 4) / 1000}
-        <br></br>
-        Created on:{" "}
-        {String(game.slice(5, 6)).substring(0, 10) +
-          " at: " +
-          String(game.slice(5, 6)).substring(11, 20)}
-        <br></br>
-      </div>
-    );
-  });
-  // <ul className="stats-list">
-  //         </ul>
-  //className="stats-button"
-  return (
-    <section>
-      <div className="GameStatus">{gameStatus === "done" && <SaveGame />}</div>
-      <div>
-        <button onClick={getGamesData}> get stats </button>
-      </div>
-      <div className="">
-        <h4>Stats: </h4>
-        {gameItems[gameItems.length - 2]}
-        ----
-        {gameItems[gameItems.length - 1]}
-        ----
-      </div>
-    </section>
->>>>>>> 8fbf088b376767ded0dd55aabbc01fbdd232ee42
   );
+  
 }
