26cf069ca159755f71aea483cd1c13910032a22f
Kent Gulka <kentgulka@modindsoft.com>
2022-04-27 10:30:28 -0600

    Demo seed data changes
    Changed the hard typing text to some Javascript code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,29 +43,15 @@ Text.create!([
   Saving up my energy (yeah, yeah, saving up my energy)'},
   {difficulty: 'medium', content: '
   We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.--That to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed, --That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn, that mankind are more disposed to suffer, while evils are sufferable, than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security.--Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world.'},
-  {difficulty: 'hard', content: 'Merchant of Syracuse, plead no more;
-  I am not partial to infringe our laws:
-  The enmity and discord which of late
-  Sprung from the rancorous outrage of your duke
-  To merchants, our well-dealing countrymen,
-  Who wanting guilders to redeem their lives
-  Have seald his rigorous statutes with their bloods,
-  Excludes all pity from our threatening looks.
-  For, since the mortal and intestine jars
-  Twixt thy seditious countrymen and us,
-  It hath in solemn synods been decreed
-  Both by the Syracusians and ourselves,
-  To admit no traffic to our adverse towns Nay, more,
-  If any born at Ephesus be seen
-  At any Syracusian marts and fairs;
-  Again: if any Syracusian born
-  Come to the bay of Ephesus, he dies,
-  His goods confiscate to the dukes dispose,
-  Unless a thousand marks be levied,
-  To quit the penalty and to ransom him.
-  Thy substance, valued at the highest rate,
-  Cannot amount unto a hundred marks;
-  Therefore by law thou art condemned to die.'}
+  {difficulty: 'hard', content: '//session.argv
+  for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i] > 0 && !isNaN(process.argv[i])) {
+      setTimeout(() => {
+        process.stdout.write(".");
+        process.stdout.write("\x07");
+      }, 1000 * process.argv[i]);
+    }
+  }'}
 ])
 
 puts "created #{Text.count} texts"
